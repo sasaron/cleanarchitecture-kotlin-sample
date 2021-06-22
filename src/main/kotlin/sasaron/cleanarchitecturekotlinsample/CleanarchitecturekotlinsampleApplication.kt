2a2aaabbecd3bc9af3be94861d0dd2346fc971c5
sasaron <mac1301010162sasakir@gmail.com>
2021-06-22 23:44:15 +0900

    first commit

---
+++ src/main/kotlin/sasaron/cleanarchitecturekotlinsample/CleanarchitecturekotlinsampleApplication.kt
@@ -0,0 +1,11 @@
+package sasaron.cleanarchitecturekotlinsample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CleanarchitecturekotlinsampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<CleanarchitecturekotlinsampleApplication>(*args)
+}
---
+++ src/test/kotlin/sasaron/cleanarchitecturekotlinsample/CleanarchitecturekotlinsampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package sasaron.cleanarchitecturekotlinsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CleanarchitecturekotlinsampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
