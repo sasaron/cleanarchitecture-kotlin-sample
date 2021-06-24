a5b86cbd4af40a3dbdbf89abd64462c525d0b6ed
sasaron <mac1301010162sasakir@gmail.com>
2021-06-24 23:57:08 +0900

    init gradle

--- src/main/kotlin/sasaron/cleanarchitecturekotlinsample/infrastructure/CleanarchitecturekotlinsampleApplication.kt
+++ src/main/kotlin/sasaron/cleanarchitecturekotlinsample/infrastructure/CleanarchitecturekotlinsampleApplication.kt
@@ -2,8 +2,10 @@ package sasaron.cleanarchitecturekotlinsample
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootApplication
+@EnableSwagger2
 class CleanarchitecturekotlinsampleApplication
 
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/sasaron/cleanarchitecturekotlinsample/infrastructure/openapi/SwaggerConfigure.kts
@@ -0,0 +1,34 @@
+package sasaron.dddkotlinsample.infrastucture
+
+import com.google.common.base.Predicates
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+@EnableSwagger2
+@Configuration
+class SwaggerConfiguration {
+    @Bean
+    fun swaggerDemoApi(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+            .useDefaultResponseMessages(false)
+            .select()
+            .paths(Predicates.not(PathSelectors.regex("^/error.*")))
+            .build()
+            .apiInfo(apiInfo())
+    }
+
+    private fun apiInfo(): ApiInfo {
+        return ApiInfoBuilder()
+            .title("Demo Sample API")
+            .description("Kotlin Sample API. DDD and CleanArch")
+            .version("0.0.1")
+            .build()
+    }
+
+}
