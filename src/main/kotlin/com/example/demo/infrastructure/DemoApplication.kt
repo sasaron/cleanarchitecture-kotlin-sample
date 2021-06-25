68ffa82fa857e513fb240996fa91cc75fad01aed
sasaron <mac1301010162sasakir@gmail.com>
2021-06-25 15:45:02 +0900

    boot spring

--- src/main/kotlin/com/example/demo/infrastructure/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/infrastructure/DemoApplication.kt
@@ -1,10 +1,12 @@
 package com.example.demo.infrastructure
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 @EnableSwagger2
 class DemoApplication
 
--- src/main/kotlin/com/example/demo/infrastructure/openapi/SwaggerConfigure.kt
+++ src/main/kotlin/com/example/demo/infrastructure/openapi/SwaggerConfigure.kt
@@ -7,10 +7,8 @@ import springfox.documentation.builders.PathSelectors
 import springfox.documentation.service.ApiInfo
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
-import springfox.documentation.swagger2.annotations.EnableSwagger2
 import java.util.function.Predicate
 
-@EnableSwagger2
 @Configuration
 class SwaggerConfiguration {
     @Bean
